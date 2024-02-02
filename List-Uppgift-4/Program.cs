e671bde740a0a2b7a33e02103edb79263fb637c9
HerrBergenholtz <zbdv8800@gmail.com>
2024-02-02 13:24:06 +0100

    Add project files.

---
+++ List-Uppgift-4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Uppgift_4
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] arr = { "Tjena", "Hejsan", "Ameen" };
+            Console.WriteLine(FinnsTecknetIAllaSträngar(arr, 'A'));
+        }
+        
+        static bool FinnsTecknetIAllaSträngar(string[] sträng, char tecken)
+        {
+            for (int i = 0; i < sträng.Length; i++)
+            {
+                if (sträng[i].ToLower().Contains(tecken.ToString().ToLower())) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
